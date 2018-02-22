75758dfec7c973c80b75e60d59db9500eac90f72
Ivan <pajares.105709@e.unavarra.es>
2018-02-22 18:31:07 +0100

    v1.2.8. Fin tarea 1.2

--- GestionPedidosArtesanos/src/gestionpedidosartesanos/GestionPedidosArtesanos.java
+++ GestionPedidosArtesanos/src/gestionpedidosartesanos/GestionPedidosArtesanos.java
@@ -16,6 +16,10 @@ public class GestionPedidosArtesanos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        VentanaPedidos ventanaPedidos = new VentanaPedidos();
+        ventanaPedidos.setVisible(true);
+        
     }
     
 }
